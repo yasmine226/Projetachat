05d7948f6a11ba5c82dc1a34da3d971b58d2f47b
yasmine hachicha <58233297+yasmine226@users.noreply.github.com>
2022-11-12 00:53:52 +0100

    Update Fournisseur.java

--- src/main/java/com/esprit/examen/entities/Fournisseur.java
+++ src/main/java/com/esprit/examen/entities/Fournisseur.java
@@ -43,13 +43,6 @@ public class Fournisseur implements Serializable {
     private Set<SecteurActivite> secteurActivites;
 	@OneToOne(cascade= CascadeType.ALL,fetch=FetchType.EAGER)
     private DetailFournisseur detailFournisseur;
-	 public Fournisseur( String code, String libelle, CategorieFournisseur categorieFournisseur,DetailFournisseur detailFournisseur) {
-			super();
-			this.code = code;
-			this.libelle = libelle;
-			this.categorieFournisseur = categorieFournisseur;
-			this.detailFournisseur = detailFournisseur;
-		}
 
 	public Fournisseur(Long idFournisseur, String code, String libelle) {
 		super();
